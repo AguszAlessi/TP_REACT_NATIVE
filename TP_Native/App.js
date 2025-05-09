e84167797c892f7f61863cd1eb7493637f262be2
Android Studio <devandroid@ort>
2025-05-09 12:10:07 -0300

    Tp terminado

--- TP_Native/App.js
+++ TP_Native/App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () => {
+    setContador(contador + 1);
+    console.log("aaa");
+  };
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      style={styles.fondo}
+    >
+      <View style={styles.contenedor}>
+        <Text style={styles.texto}>Contador: {contador}</Text>
+        
+        <TouchableOpacity style={styles.boton} onPress={incrementar}>
+          <Text style={styles.textoBoton}>{contador}</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  fondo: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    resizeMode: 'cover',
+  },
+  contenedor: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)', 
+  },
+  texto: {
+    fontSize: 26,
+    color: 'white',
+    marginBottom: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  boton: {
+    backgroundColor: '#00bcd4',
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  textoBoton: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '600',
   },
 });
